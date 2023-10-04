147f779feeb015eaa538a636d62d678b12d22e86
MamedovAR <a.mamedov1@g.nsu.ru>
2023-10-05 00:27:01 +0700

    ex01

---
+++ mod03/ex01/service_full_name/src/client_name.cpp
@@ -0,0 +1,72 @@
+#include <string.h>
+#include "rclcpp/rclcpp.hpp"
+#include "full_name_message/srv/full_name_sum_service.hpp"
+
+class ClientNameNode : public rclcpp::Node
+{
+public:
+    ClientNameNode() : Node("client_name")
+    {
+        client_ = create_client<full_name_message::srv::FullNameSumService>("SumFullName");
+    }
+
+    void run(const std::string &last_name, const std::string &name, const std::string &first_name)
+    {
+        auto request = std::make_shared<full_name_message::srv::FullNameSumService::Request>();
+        request->last_name = last_name;
+        request->name = name;
+        request->first_name = first_name;
+
+        while (!client_->wait_for_service(std::chrono::seconds(1)))
+        {
+            if (!rclcpp::ok())
+            {
+                RCLCPP_ERROR(get_logger(), "Interrupted while waiting for the service. Exiting.");
+                return;
+            }
+            RCLCPP_INFO(get_logger(), "Service not available, trying again...");
+        }
+
+        auto future_result = client_->async_send_request(request);
+    }
+
+private:
+    rclcpp::Client<full_name_message::srv::FullNameSumService>::SharedPtr client_;
+};
+
+int main(int argc, char *argv[])
+{
+    rclcpp::init(argc, argv);
+    auto node = std::make_shared<ClientNameNode>();
+    std::string last_name, first_name, name;
+    for(int i=argc-1; i>=argc-3; i--)
+    {
+        char k=0;
+        for(int j=0; j<strlen(argv[i]); j++)
+        {
+            if(argv[i][2]=='f')
+                if(k==1)
+                    first_name.append(&argv[i][j]);
+                else if(argv[i][j]=='=')
+                    k++;
+            else if(argv[i][2]=='l')
+                if(k==1)
+                    last_name.append(&argv[i][j]);
+                else if(argv[i][j]=='=')
+                    k++;
+            else if(argv[i][2]=='n')
+                if(k==1)
+                    name.append(&argv[i][j]);
+                else if(argv[i][j]=='=')
+                    k++;
+            else
+            {
+                std::cout << "Use --first_name, --last_name, --name\n";
+                exit(0);
+            }
+        }
+    }
+    node->run(last_name, name, first_name);
+    rclcpp::shutdown();
+    return 0;
+}
---
+++ mod03/ex01/service_full_name/src/service_name.cpp
@@ -0,0 +1,33 @@
+#include "rclcpp/rclcpp.hpp"
+#include "full_name_message/srv/full_name_sum_service.hpp"
+
+class ServiceNameNode : public rclcpp::Node
+{
+public:
+    ServiceNameNode() : Node("service_name")
+    {
+        service_ = create_service<full_name_message::srv::FullNameSumService>(
+            "SumFullName",
+            std::bind(&ServiceNameNode::calculateFullName, this,
+                      std::placeholders::_1, std::placeholders::_2));
+    }
+
+private:
+    void calculateFullName(
+        const std::shared_ptr<full_name_message::srv::FullNameSumService::Request> request,
+        std::shared_ptr<full_name_message::srv::FullNameSumService::Response> response)
+    {
+        response->full_name = request->last_name + ' ' + request->name + ' ' + request->first_name;
+    }
+
+    rclcpp::Service<full_name_message::srv::FullNameSumService>::SharedPtr service_;
+};
+
+int main(int argc, char *argv[])
+{
+    rclcpp::init(argc, argv);
+    auto node = std::make_shared<ServiceNameNode>();
+    rclcpp::spin(node);
+    rclcpp::shutdown();
+    return 0;
+}
