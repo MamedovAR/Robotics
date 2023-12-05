bf650929d099a85c63866a24e713e97cead7e684
MamedovAR <a.mamedov1@g.nsu.ru>
2023-12-06 03:37:26 +0700

    ex03

---
+++ mod05/ex03/notsam/src/description/notsam.cpp
@@ -0,0 +1,29 @@
+// #include <chrono>
+// #include <functional>
+// #include <memory>
+// #include <string>
+
+// #include "geometry_msgs/msg/transform_stamped.hpp"
+// #include "geometry_msgs/msg/twist.hpp"
+// #include "rclcpp/rclcpp.hpp"
+// #include "tf2/exceptions.h"
+// #include "tf2_ros/transform_listener.h"
+// #include "tf2_ros/buffer.h"
+
+// using namespace std::chrono_literals;
+
+// class Notsam: public rclcpp::Node
+// {
+//   public:
+//   Notsam()
+//   : Node("robot")
+//   {
+
+//   }
+// };
+
+#include <stdio.h>
+int main()
+{
+  return 0;
+}
